10a100ae77dd1fd3bd5cc44b613098124b71b0c3
KyrylIeremieiev <34677@ma-web.nl>
2023-12-09 12:44:53 +0100

    add html

---
+++ m10API/resources/views/base.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <header class="header">
+            <img class="header__logo"></img>
+            <div class="header__linkWrap">
+                <a class="header__link">Home</a>
+                <a class="header__link">Projecten</a>
+                <a class="header__link">Over Ons</a>
+                <a class="header__link">Contact</a>
+            </div>
+        </header>
+        <main class="main">
+
+        </main>
+    </body>   
+</html>

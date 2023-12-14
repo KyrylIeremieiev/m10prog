193d9aa1a171dbb0dfad10696f0ab78812e8e0dd
KyrylIeremieiev <34677@ma-web.nl>
2023-12-14 11:46:03 +0100

    add tailwind

--- m10API/resources/views/base.blade.php
+++ m10API/resources/views/base.blade.php
@@ -1,16 +1,24 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    </head>
+    
     <body>
-        <header class="header">
-            <img class="header__logo"></img>
-            <div class="header__linkWrap">
-                <a class="header__link">Home</a>
-                <a class="header__link">Projecten</a>
-                <a class="header__link">Over Ons</a>
-                <a class="header__link">Contact</a>
-            </div>
-        </header>
-        <main class="main">
+    <header class="flex justify-between items-center p-8">
+        <img class="w-40" src="{{ asset('img/logo-gemeente-amsterdam.png') }}" alt="Logo">
+        <div class="flex items-center text-red-500 text-lg space-x-6">
+            <a class="hover:underline" href="#">Home</a>
+            <a class="hover:underline" href="#">Projecten</a>
+            <a class="hover:underline" href="#">Over Ons</a>
+            <a class="hover:underline" href="#">Contact</a>
+        </div>
+    </header>
+    <main class="main">
 
-        </main>
-    </body>   
+    </main>
+</body>
+
 </html>
